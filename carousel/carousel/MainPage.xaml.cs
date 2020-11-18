8e36e28b19cfa45f38185933a069b6e890d3c0d2
Opilane TTHK <opilane@tthk.ee>
2020-11-18 11:28:57 +0200

    Добавление информации и кнопок

--- carousel/carousel/MainPage.xaml.cs
+++ carousel/carousel/MainPage.xaml.cs
@@ -1,22 +1,70 @@
-﻿using GalaSoft.MvvmLight.Command;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Input;
-using WECPOFLogic;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace carousel
 {
     public partial class MainPage : CarouselPage
     {
-        CommandBinding commandBinding;
+        Button btnTest, btn1, btn2, btn3, btn4;
+        Label holerikLBL, flegmatikLBL, melanholikLBL;
+        Uri uri;
         public MainPage()
         {
             //InitializeComponent();
+
+            btnTest = new Button
+            {
+                Text = "Тест на темперамент"
+            };
+            btnTest.Clicked += BtnTest_Clicked;
+
+            btn1 = new Button
+            {
+                Text = "Описание",
+                ImageSource = "holerik.jpg"
+            };
+            btn1.Clicked += Btn1_Clicked;
+
+            btn2 = new Button
+            {
+                Text = "Описание",
+                ImageSource = "flegmatik.png"
+            };
+            btn2.Clicked += Btn2_Clicked;
+
+            btn3 = new Button
+            {
+                Text = "Описание",
+                ImageSource = "melanholik.jpg"
+            };
+            btn3.Clicked += Btn3_Clicked;
+
+            holerikLBL = new Label
+            {
+                IsVisible = false,
+                Padding = 20,
+                Text = "Холерик — это эмоционально - активный тип темперамента с неустойчивой нервной системой, склонный к лидерству и доминированию. У холериков часто меняется настроение, поэтому они не предсказуемы. Без движения им очень скучно, поэтому холерики берут на себя много дел, чтобы почувствовать суету и ответственность. Люди с данным типом темперамента стремятся успеть везде дома, на работе, в дружбе и т.д. "
+            };
+
+            flegmatikLBL = new Label
+            {
+                IsVisible = false,
+                Padding = 20,
+                Text = "Флегматики - это  сдержанные люди,  не склонные к риску и  невозмутимы в любой ситуации. Прежде чем принять решение, они обдумывают всё до мелочей. Флегматик всегда сдержанно выражает свои эмоции. Они стараются избежать конфликта, поэтому могут уступить в споре. Представители этого типа, видят жизнь в реальных красках. Именно поэтому  их цели в большинстве случаев достигаются. "
+            };
+
+            melanholikLBL = new Label
+            {
+                IsVisible = false,
+                Padding = 20,
+                Text = "Меланхолик - это пассивный тип темперамента, которому свойственны глубокие переживания на любые события, высокая чувствительность, обидчивость и эмоциональность. "
+            };
 
             var startPage = new ContentPage
             {
@@ -38,8 +86,8 @@ namespace carousel
                             Source = "temperametri.gif",
                             HeightRequest = 350,
                             WidthRequest = 350
-                        }
-                        
+                        },
+                        btnTest
                     }
                 }
             };
@@ -63,11 +111,7 @@ namespace carousel
                             HeightRequest = 250,
                             WidthRequest = 250
                         },
-                        new Label
-                        {
-                            Padding = 20,
-                            Text = "Холерик — это эмоционально - активный тип темперамента с неустойчивой нервной системой, склонный к лидерству и доминированию. У холериков часто меняется настроение, поэтому они не предсказуемы. Без движения им очень скучно, поэтому холерики берут на себя много дел, чтобы почувствовать суету и ответственность. Люди с данным типом темперамента стремятся успеть везде дома, на работе, в дружбе и т.д. "
-                        }
+                        holerikLBL
                     }
                 }
             };
@@ -91,16 +135,7 @@ namespace carousel
                             HeightRequest = 250,
                             WidthRequest = 250
                         },
-                        new Button
-                        { 
-                            Text = "Описание",
-                            
-                        },
-                        new Label
-                        {
-                            Padding = 20,
-                            Text = "Флегматики - это  сдержанные люди,  не склонные к риску и  невозмутимы в любой ситуации. Прежде чем принять решение, они обдумывают всё до мелочей. Флегматик всегда сдержанно выражает свои эмоции. Они стараются избежать конфликта, поэтому могут уступить в споре. Представители этого типа, видят жизнь в реальных красках. Именно поэтому  их цели в большинстве случаев достигаются. "
-                        }
+                        flegmatikLBL
                     }
                 }
             };
@@ -124,11 +159,7 @@ namespace carousel
                             HeightRequest = 250,
                             WidthRequest = 250
                         },
-                        new Label
-                        {
-                            Padding = 20,
-                            Text = "Меланхолик - это пассивный тип темперамента, которому свойственны глубокие переживания на любые события, высокая чувствительность, обидчивость и эмоциональность. "
-                        }
+                        
                     }
                 }
             };
@@ -166,6 +197,27 @@ namespace carousel
             Children.Add(flegmatikPage);
             Children.Add(melanholikPage);
             Children.Add(sangvinikPage);
+        }
+
+        private void Btn3_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Btn2_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Btn1_Clicked(object sender, EventArgs e)
+        {
+            holerikLBL.IsVisible = true;
+        }
+
+        private async void BtnTest_Clicked(object sender, EventArgs e)
+        {
+            uri = new Uri("https://testometrika.com/personality-and-temper/the-formula-of-temperament-a-belov/");
+            await Browser.OpenAsync(uri);
         }
     }
 }
